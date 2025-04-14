b1d136c0079752a7ff36acbc03954f649ff80ca1
vesaone <codypeisley95@hotmail.com>
2025-04-14 20:21:19 +1000

    updates

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",        // includes root HTML
-    "./src/**/*.{js,jsx,ts,tsx}", // includes all React component files
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
